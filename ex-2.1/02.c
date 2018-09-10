d536230b44d7706f6aa653f702da08d75935257c
rodrigojat <rodrigojat@hotmail.com>
2018-09-10 12:37:14 -0300

    Update 02.c

--- ex-2.1/02.c
+++ ex-2.1/02.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main () {
+	int a,b;
+	printf("Digite valor de A:\n");
+	scanf("%d",&a);
+	printf("Digite valor de B:\n");
+	scanf("%d",&b);
+	if (a>b)
+		printf("maior valor %d!",a);
+	else
+		printf("maior valor %d!",b);
+}
