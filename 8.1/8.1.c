0df2d6e96d12f67ff629b47c415de2f49a0e04a9
rodrigojat <rodrigojat@hotmail.com>
2018-11-28 15:11:10 -0200

    Create 8.1.c

---
+++ 8.1/8.1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int i,tamanho;
+    char frase[25];
+    
+    FILE* arq = fopen("8.1.txt","w");
+
+    printf("Digite a frase(sem espaços): \n");
+    scanf("%s",frase);
+    tamanho = strlen(frase);
+
+
+    for(i=0;i<(tamanho);i++)
+    {
+        fputc(frase[i],arq);
+    }
+    
+    fclose(arq);
+    return 0;
+}
